ee648e5a85bff9d457ad3ec329dcd5cae7ebfc44
Vinicius Augusto <vinicius.augusto0704@gmail.com>
2023-03-19 13:36:50 -0300

    ajustes tela cadastro

--- fiapatividade/src/main/java/com/example/fiapatividade/Cadastro_Usuario.kt
+++ fiapatividade/src/main/java/com/example/fiapatividade/Cadastro_Usuario.kt
@@ -10,17 +10,14 @@ class Cadastro_Usuario : AppCompatActivity() {
     @SuppressLint("MissingInflatedId", "WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fiap_login)
+        setContentView(R.layout.activity_fiap_cadastro)
 
+        // TODO: Exemplo de pegar valor de um campo
         //val username = findViewById<EditText>(R.id.username)
-        //val password = findViewById<EditText>(R.id.password)
-        val myButton = findViewById<Button>(R.id.login_portugues)
-        //val botao = Button("Apertar")
-        myButton.setOnClickListener {
-
-            //Toast.makeText(this, "Botão encontrado!", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, Login_Usuario::class.java)
-            startActivity(intent)
+        val loginButton = findViewById<Button>(R.id.login_button)
+        loginButton.setOnClickListener {
+            val loginTela = Intent(this, Login_Usuario::class.java)
+            startActivity(loginTela)
 
         }
 
--- fiapatividade/src/main/java/com/example/fiapatividade/Login_Usuario.kt
+++ fiapatividade/src/main/java/com/example/fiapatividade/Login_Usuario.kt
@@ -1,12 +1,22 @@
 package com.example.fiapatividade
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class Login_Usuario: AppCompatActivity() {
+    @SuppressLint("WrongViewCast", "MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fiap_maps)
+        setContentView(R.layout.activity_fiap_login)
 
+        val cadastroButton = findViewById<Button>(R.id.cadastro_button)
+        cadastroButton.setOnClickListener {
+            val intent = Intent(this, Cadastro_Usuario::class.java)
+            startActivity(intent)
+
+        }
     }
 }
